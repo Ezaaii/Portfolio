e394fbc7fac046b79e3a8889ac1fc88fc94f043d
Unknown <n.vanroost@gmail.com>
2017-11-17 12:18:12 +0100

    update scroll + buttons

--- scroll.js
+++ scroll.js
@@ -11,40 +11,71 @@ var docelink = document.querySelector("#codelink");
 var code = document.querySelector("#code");
 var contlink = document.querySelector("#contactlink");
 var cont = document.querySelector("#contact");
-
+//refresh scroll + highlight
+homelink.classList.add("highlighted");
+window.scroll({
+  top : home.offsetTop,
+  left : 0,
+  behavior : 'smooth'
+});
 
 homelink.addEventListener("click", function(){
+  homelink.classList.add("highlighted");
   window.scroll({
     top : home.offsetTop,
     left : 0,
     behavior : 'smooth'
   });
+  illulink.classList.remove("highlighted");
+  animlink.classList.remove("highlighted");
+  codelink.classList.remove("highlighted");
+  contlink.classList.remove("highlighted");
 });
 illulink.addEventListener("click", function(){
+  illulink.classList.add("highlighted");
   window.scroll({
     top : illu.offsetTop,
     left : 0,
     behavior : 'smooth'
   });
+  homelink.classList.remove("highlighted");
+  animlink.classList.remove("highlighted");
+  codelink.classList.remove("highlighted");
+  contlink.classList.remove("highlighted");
 });
 animlink.addEventListener("click", function(){
+  animlink.classList.add("highlighted");
   window.scroll({
     top : anim.offsetTop,
     left : 0,
     behavior : 'smooth'
   });
+  homelink.classList.remove("highlighted");
+  illulink.classList.remove("highlighted");
+  codelink.classList.remove("highlighted");
+  contlink.classList.remove("highlighted");
 });
 codelink.addEventListener("click", function(){
+  codelink.classList.add("highlighted");
   window.scroll({
     top : code.offsetTop,
     left : 0,
     behavior : 'smooth'
   });
+  homelink.classList.remove("highlighted");
+  illulink.classList.remove("highlighted");
+  animlink.classList.remove("highlighted");
+  contlink.classList.remove("highlighted");
 });
 contlink.addEventListener("click", function(){
+  contlink.classList.add("highlighted");
   window.scroll({
     top : cont.offsetTop,
     left : 0,
     behavior : 'smooth'
   });
+  homelink.classList.remove("highlighted");
+  illulink.classList.remove("highlighted");
+  animlink.classList.remove("highlighted");
+  codelink.classList.remove("highlighted");
 });
